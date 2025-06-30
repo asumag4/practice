5ebf7631330e316dc59d72bcde5bbbee6166f253
azkal7 <136876816+asumag4@users.noreply.github.com>
2025-06-29 18:27:26 -0600

    2724, learning how sort() and toSorted() functions work

---
+++ LeetCode/JavaScript/30_days_js/2724.Sort_by.js
@@ -0,0 +1,35 @@
+// *** MY ATTEMPTS ***
+
+// This is the correct answer
+/*
+var sortBy = function(arr, fn) {
+    return arr.toSorted((a,b) => fn(a) - fn(b));
+};
+*/
+
+/*
+var sortBy = function(arr, fn) {
+    return arr.toSorted((a,b) => fn(b) - fn(a));
+};
+
+
+var sortBy = function(arr, fn) {
+    return arr.sort(function(a,b){
+        fn(b) - fn(a)
+    })
+}
+
+var sortBy = function(arr, fn) {
+    return arr.toSorted(function(a,b){
+        fn(b) - fn(a)
+    })
+}
+*/
+
+// Also correct answer
+var sortBy = function(arr, fn) {
+    return arr.sort(function(a,b){
+        return fn(a) - fn(b)
+    })
+}
+
