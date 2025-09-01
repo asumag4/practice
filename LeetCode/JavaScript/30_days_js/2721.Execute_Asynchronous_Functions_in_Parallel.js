adba8cdc35773287aa9d337e025fca0636df0cc0
azkal7 <136876816+asumag4@users.noreply.github.com>
2025-09-01 11:54:36 -0600

    Q.2721

--- LeetCode/JavaScript/30_days_js/2721.Execute_Asynchronous_Functions_in_Parallel.js
+++ LeetCode/JavaScript/30_days_js/2721.Execute_Asynchronous_Functions_in_Parallel.js
@@ -2,29 +2,24 @@
  * @param {Array<Function>} functions
  * @return {Promise<any>}
  */
-var promiseAll = function(functions) {
+var promiseAll = async function(functions) {
+    return new Promise ( (resolve, reject ) => {
+        const results = new Array(functions.length);
+        let counter = 0;
 
-    // Init a new promise obj to be returned 
-    return new Promise ( (resolve, reject)  => {
-
-    //  Create a new array that is the Array of the lengths of the functions 
-    const arr1 = new Array(functions.length);
-    //  Initiate a counter at 0 
-    const counter = 0;
-    //  For each function in functions
-    functions.forEach( (fn, i) => {
-    //      initate the function
-        fn().
-    //      use .then() clause, where the value  is appened to your holder array at the respective index
-        then( val => { 
-            arr1[i] = val;
-    //      iterate the counter
-            counter++; 
-    //      Now evaluate, if we're at the end of the functions array, resolve the results
-            if (count === functions.length) resolve(results);
-            };
-    //      Under the .then() clause, have .catch() clause for where the reason is caught as a rejection
-        ).catch(reason => reject(reason));
-        });
+        functions.forEach( (fn, i) => {
+            fn()
+            .then( val => {
+                results[i] = val;
+                counter++;
+                if (counter == functions.length) resolve(results);
+            })
+            .catch(reason => reject(reason))
+        })
     });
 };
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
