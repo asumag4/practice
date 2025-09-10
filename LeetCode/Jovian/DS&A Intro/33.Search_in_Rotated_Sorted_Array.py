699b3b213c38ba448fa14e8f521c56a22a3d2b50
azkal7 <136876816+asumag4@users.noreply.github.com>
2025-09-09 21:22:16 -0600

    Q.33

--- LeetCode/Jovian/DS&A Intro/33.Search_in_Rotated_Sorted_Array.py
+++ LeetCode/Jovian/DS&A Intro/33.Search_in_Rotated_Sorted_Array.py
@@ -1,55 +1,39 @@
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
-            # We need to levarage the values of high and min=
-            # When lo > hi, we've found a critical point
+    def search(self, nums: List[int], target: int) -> int:
+        
+        # Start the binary search
+        lo = 0 
+        hi = len(nums) - 1
+
+        # Init a while loop if lo and hi positions stil make sense
+        while (lo <= hi):
+
+        # Calc the mid 
+            mid = (lo + hi) // 2
+
+            # Assess if were at the target, then return mid 
+            if (nums[mid] == target):
+                return mid
             
-            lo = 0
-            hi = len(nums) - 1
-            # idx = -1
+            # Now we need to find the range we need to look for 
 
-            # print(lo) # DEBUG
-            # print(hi) # DEBUG
-            if (not nums):
-                return -1
-            
-            # We also need to leverage the the +1 and -1 values of the middle position 
-            while (lo <= hi):
-                
-                mid = (hi + lo) // 2
-                
-                print(nums[lo]) # DEBUG
-                print(nums[mid]) # DEBUG
-                print(nums[hi]) # DEBUG
-
-                if (hi == lo):
-                    if ((nums[hi] == target) and (nums[lo] == target)):
-                        return mid
-                    else:
-                        return -1
-
-                elif (nums[mid] == target):
-                    return mid
-                
-                # If target is within lo -> mid-1, go lower 
-                elif ( (nums[lo] <= target) and (nums[mid-1] >= target) ):
-                    hi = mid - 1 
-
-                # If target is within mid+1 -> hi, go higher 
-                elif ( (nums[mid+1] <= target) and (nums[hi] >= target) ):
-                    lo = mid + 1
-
+            # If the lo to mid portion is organized, then;
+            elif (nums[lo] <= nums[mid]):
+                # See if the target is within that range; if so go left
+                if (nums[lo] <= target <= nums[mid]):
+                    hi = mid - 1
+                # If not then go right
                 else:
-                    return -1
+                    lo = mid + 1
             
-sol = Solution()
-# print(sol.search([4,5,6,7,0,1,2], 0))
-# print(sol.search([1,2,3,4,5,6,7,8,9,10], 3))
-# print(sol.search([6,7,8,9,10,1,2,3,4,5], 3))
-# print(sol.search([3,4,5,1,2,3], 3))
-# print(sol.search([1,2,5,6,7], 7))
-# print(sol.search([1,2,5,6,7], 7))
-# print(sol.search([4,5,6,7,0,1,2], 3))
-# print(sol.search([3,4,5,1,2,3], 6))
-# print(sol.search([1,2,3,4,5,6,7,8,9,10], -1))
-
-print(sol.search([1,3], 2))
+            # If the mid to hi portion is organized, then;
+            elif (nums[mid] <= nums[hi]):
+                # See if the target is within that range; if so go right
+                if (nums[mid] <= target <= nums[hi]):
+                    lo = mid + 1
+                # If not then go left
+                else:
+                    hi = mid - 1
+        
+        # If not found the terminate then wait until while loop terminates and return -1 
+        return -1
