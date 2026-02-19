c3eede88183f5a3166b3df9b334b228f69b34e7c
asumag4 <sumaguearthur@outlook.com>
2026-02-18 21:43:01 -0700

    q200, q219

--- LeetCode/70 Leetcode Problems in 5+ Hours/Arrays (two pointer)/219.contains_duplicate_II.py
+++ LeetCode/70 Leetcode Problems in 5+ Hours/Arrays (two pointer)/219.contains_duplicate_II.py
@@ -1,30 +1,17 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        
-        i = 0
-        j = 1
 
-        while (
-            i <= len(nums) - 2 and
-            j <= len(nums) - 1
-        ):
-            while (j <= (len(nums) - 1)):
+        for i, num in enumerate(nums):
 
-                if (nums[i] == nums[j]):
-
-                    if (abs(i - j) <= k):
-                        return True
-                    else: 
-                        i += 1
-                        j = i + 1
-                    
-                else: 
-                    j += 1
-            
-            i += 1
-            j = i + 1
-        
-        return False
+            if num in nums[i+1:]:
+                print(f"Found {num} in {nums[i+1:]}")
+                j = nums[i+1:].index(num)
+                j += i + 1
+                if abs(i-j) <= k:
+                    print(i, j)
+                    return True
+                else:
+                    continue
     
 sol = Solution()
 print(sol.containsNearbyDuplicate(nums = [1,2,3,1], k = 3))
--- LeetCode/70 Leetcode Problems in 5+ Hours/Arrays/200.number_of_islands.py
+++ LeetCode/70 Leetcode Problems in 5+ Hours/Arrays/200.number_of_islands.py
@@ -1,55 +1,4 @@
-from collections import deque
-
-class Solution:
-    def numIslands(self, grid: list[list[str]]) -> int:
-        
-        traversed = []
-        counter = 0  
-        x_len = len(grid[0]) - 1
-        y_len = len(grid) - 1
-
-        def scopeIsland(x, y):
-
-            q = deque([(x,y)])
-
-            while q:
-                
-                print(q)
-                n_x, n_y = q.popleft()
-
-                for dir_x, dir_y in ([(-1,0), (0,1), (1,0), (0,-1)]):
-
-                    if (n_x + dir_x, n_y + dir_y) in traversed:
-                        continue
-                    else: 
-                        if (
-                            n_x < x_len and
-                            n_x > 0 and
-                            n_y < y_len and
-                            n_y > 0
-                        ):
-                            if (grid[n_x + dir_x][n_y + dir_y] == "1"):
-                                q.append((n_x + dir_x, n_y + dir_y))
-                            traversed.append((n_x + dir_x, n_y + dir_y))
-            
-
-        for x in range(0,len(grid)):
-            for y in range(0, len(grid[0])):
-
-                # print(grid[i][j])
-
-                if (x,y) in traversed:
-                    continue
-                else:
-                    if (grid[x][y] == "1"): 
-                        # print("Found an island!")
-                        counter += 1
-                        scopeIsland(x,y)
-                    else: 
-                        traversed.append((x,y))
-
-        return counter
-        
+# Failed attempt #3
 
 sol = Solution()
 print(sol.numIslands([
@@ -58,9 +7,9 @@ print(sol.numIslands([
   ["1","1","0","0","0"],
   ["0","0","0","0","0"]
 ])) # 1
-print(sol.numIslands([
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-])) # 3
+# print(sol.numIslands([
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ])) # 3
