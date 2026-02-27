591aeb47708f44aa9eb67131ecf514d86e25e15a
asumag4 <sumaguearthur@outlook.com>
2026-02-26 22:55:48 -0700

    q.53

--- LeetCode/70 Leetcode Problems in 5+ Hours/Back Tracking/53.maximum_subarray.py
+++ LeetCode/70 Leetcode Problems in 5+ Hours/Back Tracking/53.maximum_subarray.py
@@ -1,39 +1,19 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        # Start at a = 0 and b = 0 for the case of when there's only one item in the list
-        a = 0
-        b = 0
-        # Grab only from one position because we don't want to res = nums[0] * 2
-        res = nums[a] 
-        curr = res
+        # Have reference vars
+        tot = 0
+        maxSub = nums[0]
 
-        # Need to make this recursive: 
-        # need to assess between the current iteration vs. the proposed push
-        def push(jab, pivot, contend):
-            if (jab )
-            return sum(nums[n_a, n_b + 1])
+        # Go through each int
+        for i in nums:
 
-        # We'll use a Greedy Approach, assess += 1 for a or b, go towards the step that increases (+) the most
-        while (
-            # Placement conditionals
-            (b <= len(nums) - 1) and 
-            (a <= b) and
-            # sum-based conditionals
-            ):
+            # print(f"At: {i}, Tot: {tot}, maxSub {maxSub}") # DEBUG
+
+            if tot < 0:
+                tot = 0
             
-            if ((a + 1) <= b):
-                a_push = push(a+1, b)
-            if (b <= len(nums)):
-                b_push = push(a, b+1)
-            
-            if (a_push > b_push):
-                a += 1
-                res = max(res, a_push)
-            else:
-                b += 1
-                res = max(res, b_push)
+            tot += i
+            maxSub = max(maxSub, tot)
 
-            # But if the += 1 step for either a or b is smaller than the current number,
-
-            # Look at traversing first on the largest number
+        return maxSub
